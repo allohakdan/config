468c5a4f0f7a9e3d7d23f1d152bd7cb50bdde8d0
Dan Brooks <mrdanbrooks@gmail.com>
2013-10-11 15:49:51 -0400

    [db] updates macports software to auto install

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,7 +2,10 @@
 echo "Installing git,vim,tmux,ssh from apt"
 if [[ `uname` == "Linux" ]]; then
     sudo apt-get install git-core vim tmux ssh exuberant-ctags
+elif [[ `uname` == "Darwin" ]]; then
+    sudo port install wget tmux python27 watch htop iftop vim
 fi
+
 echo "Installing chdan into .bashrc"
 echo "alias chdan='source ~/.dan/.bashrc'" >> ~/.bashrc
 
