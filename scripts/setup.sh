0385e836135dee65c13524f8e5c2b8dd426007c1
dan brooks <dan@danbrooks.net>
2015-09-26 22:20:48 -0400

    change dl to install via pip!

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 if [[ `uname` == "Linux" ]]; then
     echo "Installing git,vim,tmux,ssh,pylint,flake8 from apt"
-    sudo apt-get install git-core vim tmux ssh exuberant-ctags pylint python-flake8
+    sudo apt-get install git-core vim tmux ssh exuberant-ctags pylint python-flake8 python-pip
 elif [[ `uname` == "Darwin" ]]; then
     # Test if at least these things are installed, python and vim report more then once...
-    if [[ `port installed active | cut -d @ -f 1 | egrep -c " wget | tmux | python27 | py27-pylint | watch | htop | iftop | vim | gnupg | py27-flake8 "` -ge 10 ]]; then
+    if [[ `port installed active | cut -d @ -f 1 | egrep -c " wget | tmux | python27 | py27-pylint | watch | htop | iftop | vim | gnupg | py27-flake8 | py27-pip"` -ge 11 ]]; then
         echo "Macport packages already installed, skipping"
     else
         echo "Installing wget,tmux,python,pylint,watch,htop,iftop,vim,gnupg,flake8 from macports"
-        sudo port install wget tmux python27 py27-pylint watch htop iftop vim gnupg py27-flake8
+        sudo port install wget tmux python27 py27-pylint watch htop iftop vim gnupg py27-flake8 py27-pip
         sudo port install vim +python27
         sudo port select --set python python27
         sudo port select --set pylint pylint27
         sudo port select --set flake8 flake827
+        sudo port select --set pip pip27
     fi
 fi
 
@@ -28,9 +29,10 @@ if command -v dl >/dev/null 2>&1; then
     echo "dl has already been installed, skipping"
 else
     echo "Installing dl script"
-    cd /usr/local/bin
-    sudo -E wget --no-check-certificate http://raw.github.com/allohakdan/dl/master/dl
-    sudo chmod 755 dl
+    sudo pip install dl
+#     cd /usr/local/bin
+#     sudo -E wget --no-check-certificate http://raw.github.com/allohakdan/dl/master/dl
+#     sudo chmod 755 dl
 fi
 
 # Install Vim Scripts
