da7c8c8862d26e678e763183e1f7171aff9c0532
dan brooks <dan@danbrooks.net>
2017-01-11 16:33:12 -0500

    yml parsing stuff

---
+++ .parse_yaml.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+# https://gist.github.com/pkuczynski/8665367
+#
+#config.yml: 
+#-----------
+#  development:
+#    adapter: mysql2
+#    encoding: utf8
+#    database: my_database
+#    username: root
+#    password:
+#
+# $(parse_yaml config.yml "prefix_")
+# echo $config_development_database
+#
+# Find all variables for Linux
+# export $(parse_yaml config.yml "config_")
+# compgen -A variable | grep "config_.*_Linux"
+parse_yaml() {
+   local prefix=$2
+   local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
+   sed -ne "s|^\($s\)\($w\)$s:$s\"\(.*\)\"$s\$|\1$fs\2$fs\3|p" \
+        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
+   awk -F$fs '{
+      indent = length($1)/2;
+      vname[indent] = $2;
+      for (i in vname) {if (i > indent) {delete vname[i]}}
+      if (length($3) > 0) {
+         vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
+         printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
+      }
+   }'
+}
