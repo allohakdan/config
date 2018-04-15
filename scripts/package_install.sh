c3b790491228b2b2d64972a7f9e5b0f310b6a5a8
dan brooks <dan@danbrooks.net>
2018-04-15 09:49:35 -0400

    updates for bash install system

--- scripts/package_install.sh
+++ scripts/package_install.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 source ../.parse_yaml.sh
-eval $(parse_yaml packages.yml "config_")
+# eval $(parse_yaml packages.yml "config_")
+eval $(filter_yaml packages.yml "config_" "Darwin")
 echo  "$config_git_Linux"
+echo "$config_vim_Raspbian8"
 
+echo "--------------"
 for var in ${!config_@}; do
-#     printf "%s%q\n" "$var=" "${!var}"
+    printf "%s%q\n" "$var=" "${!var}"
     printf "%q\n" "${!var}"
 done
+
+# TODO write a new version of parse_yaml that filters for the platform name and only returns those variables.
+
 
 #compgen -v | grep "config_" | echo
 # compgen -A variable | grep "config_" | 
